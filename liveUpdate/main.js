53d0460d417a37c8e3748fbc4939981bf0aa6d5f
ganto1812 <ganto1812@gmail.com>
2019-01-21 21:17:09 +0000

    add your own pic

--- liveUpdate/main.js
+++ liveUpdate/main.js
@@ -9,3 +9,19 @@ function handleUpdate() {
 }
 inputs.forEach(input => input.addEventListener("change", handleUpdate));
 inputs.forEach(input => input.addEventListener("mousemove", handleUpdate));
+
+function uploadImage(input) {
+  let image = document.querySelector("#editImage");
+  if (input.files && input.files[0]) {
+    let reader = new FileReader();
+    reader.onload = function(e) {
+      image.src = e.target.result;
+      image.width = "200";
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+// function download(image) {
+
+// }

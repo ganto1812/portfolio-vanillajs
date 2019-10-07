389206ed6f7703f1b248dade19ddcb1e1931b6ed
Anto <ganto1812@gmail.com>
2019-10-06 23:18:07 -0300

    added tribute page

---
+++ tributePage/tribute.js
@@ -0,0 +1,10 @@
+
+function liked()
+{   
+document.getElementById("like").innerHTML = "liked";
+  document.getElementById("like").style.color = "#adc2eb";
+  alert("THANK YOU FOR THE LIKE :)");
+}
+
+footer.style.fontSize = "12px";
+footer.style.textAlign = "right";
